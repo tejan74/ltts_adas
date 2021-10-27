f96068f8bed7448f2439acc874d712ac43b79fc3
keshavadk <keshavadk@gmail.com>
2021-10-27 18:44:53 +0530

    Updated code on reset password

--- cvat/settings/development.py
+++ cvat/settings/development.py
@@ -20,14 +20,14 @@ SENDFILE_BACKEND = 'sendfile.backends.development'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#     'ENGINE': 'djongo',
-#     'NAME': 'new_db',
-#     'HOST': '127.0.0.1',
-#     'PORT': 27017,
-#     }
-# }
+DATABASES = {
+    'default': {
+    'ENGINE': 'djongo',
+    'NAME': 'new_db',
+    'HOST': '127.0.0.1',
+    'PORT': 27017,
+    }
+}
 
 
 
@@ -48,3 +48,15 @@ CORS_ORIGIN_WHITELIST = [UI_URL]
 CORS_REPLACE_HTTPS_REFERER = True
 
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'adasltts@gmail.com'
+EMAIL_HOST_PASSWORD = 'ibahsrokbbtuzsxs'
+
+# EMAIL_HOST_USER = 'savitakulkarni76@gmail.com'
+# EMAIL_HOST_PASSWORD = 'svofzbnpuemoihyf'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Password reset mail from ADAS team <noreply@ltts.com>'

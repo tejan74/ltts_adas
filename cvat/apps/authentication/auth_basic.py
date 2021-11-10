a2ec55b83028706fa18fc9e4bf315535ee86e8dd
Savita05 <savitakulkarni76@gmail.com>
2021-11-10 13:49:28 +0530

    session id

--- cvat/apps/authentication/auth_basic.py
+++ cvat/apps/authentication/auth_basic.py
@@ -11,14 +11,15 @@ from . import AUTH_ROLE
 def create_user(sender, instance, created, **kwargs):
     from django.contrib.auth.models import Group
 
-    if instance.is_superuser and instance.is_staff:
-        db_group = Group.objects.get(name=AUTH_ROLE.ADMIN)
-        instance.groups.add(db_group)
+    # if instance.is_superuser and instance.is_staff:
+    #     db_group = Group.objects.get(name=AUTH_ROLE.ADMIN)
+    #     instance.groups.add(db_group)
 
-        # create and verify EmailAddress for superuser accounts
-        if allauth_settings.EMAIL_REQUIRED:
+
+    # for group_name in settings.DJANGO_AUTH_DEFAULT_GROUPS:
+    #     db_group = Group.objects.get(name=getattr(AUTH_ROLE, group_name))
+    #     instance.groups.add(db_group)
+
+    # create and verify EmailAddress for superuser accounts
+    if allauth_settings.EMAIL_REQUIRED:
             EmailAddress.objects.get_or_create(user=instance, email=instance.email, primary=True, verified=True)
-
-    for group_name in settings.DJANGO_AUTH_DEFAULT_GROUPS:
-        db_group = Group.objects.get(name=getattr(AUTH_ROLE, group_name))
-        instance.groups.add(db_group)

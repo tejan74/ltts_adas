fb9acfaeccc826881496f771c3b685b17eb697c4
gitibharatibiswal <gitibharati@gmail.com>
2021-11-17 16:28:40 +0530

    added tasks-page

--- cvat-ui/src/components/tasks-page/empty-list.tsx
+++ cvat-ui/src/components/tasks-page/empty-list.tsx
@@ -9,8 +9,14 @@ import { Row, Col } from 'antd/lib/grid';
 import Icon from '@ant-design/icons';
 
 import { EmptyTasksIcon } from 'icons';
+interface EmptyList_userProps {
+    user:any;
+}
 
-export default function EmptyListComponent(): JSX.Element {
+
+export default function EmptyListComponent(props: EmptyList_userProps): JSX.Element {
+    const {
+        user } = props;
     return (
         <div className='cvat-empty-tasks-list'>
             <Row justify='center' align='middle'>
@@ -20,21 +26,26 @@ export default function EmptyListComponent(): JSX.Element {
             </Row>
             <Row justify='center' align='middle'>
                 <Col>
+
                     <Text strong>No tasks created yet ...</Text>
+
                 </Col>
+
             </Row>
             <Row justify='center' align='middle'>
                 <Col>
                     <Text type='secondary'>To get started with your annotation project</Text>
                 </Col>
             </Row>
+            {user.isSuperuser && (
             <Row justify='center' align='middle'>
                 <Col>
                     <Link to='/tasks/create'>create a new task</Link>
                     <Text type='secondary'> or try to </Text>
-                    {/* <Link to='/projects/create'>create a new project</Link> */}
+                    <Link to='/projects/create'>create a new project</Link>
                 </Col>
             </Row>
+             ) }
         </div>
     );
 }

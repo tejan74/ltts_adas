160e16b4754fddb33181a8e3fd776aa0289329b2
rajun918 <raju.n@ltts.com>
2021-12-23 16:21:03 +0530

    new changes

--- cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/resize-control.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/resize-control.tsx
@@ -3,8 +3,9 @@
 // SPDX-License-Identifier: MIT
 
 import React from 'react';
-import Icon from '@ant-design/icons';
-
+// import Icon from '@ant-design/icons';ZoomOutOutlined
+import Icon, { ZoomInOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import { ZoomIcon } from 'icons';
 import { ActiveControl } from 'reducers/interfaces';
 import { Canvas } from 'cvat-canvas-wrapper';
@@ -17,26 +18,42 @@ export interface Props {
 
 function ResizeControl(props: Props): JSX.Element {
     const { activeControl, canvasInstance } = props;
-
     return (
-        <CVATTooltip title='Select a region of interest' placement='right'>
-            <Icon
-                component={ZoomIcon}
-                className={
-                    activeControl === ActiveControl.ZOOM_CANVAS ?
-                        'cvat-resize-control cvat-active-canvas-control' :
-                        'cvat-resize-control'
-                }
+        <>
+            <CVATTooltip title='Select a region of interest' placement='right'>
+                <Icon
+                    component={ZoomIcon}
+                    className={
+                        activeControl === ActiveControl.ZOOM_CANVAS ?
+                            'cvat-resize-control cvat-active-canvas-control' :
+                            'cvat-resize-control'
+                    }
+                    onClick={(): void => {
+                        if (activeControl === ActiveControl.ZOOM_CANVAS) {
+                            canvasInstance.zoomCanvas(false);
+                        } else {
+                            canvasInstance.cancel();
+                            canvasInstance.zoomCanvas(true);
+                        }
+                    }}
+                />
+            </CVATTooltip>
+            <Button
+                type='link'
+                className='cvat-annotation-header-button'
                 onClick={(): void => {
-                    if (activeControl === ActiveControl.ZOOM_CANVAS) {
-                        canvasInstance.zoomCanvas(false);
+                    if (activeControl === ActiveControl.ZOOM_Button_CANVAS) {
+                        canvasInstance.zoomCanvasButton(false);
                     } else {
                         canvasInstance.cancel();
-                        canvasInstance.zoomCanvas(true);
+                        canvasInstance.zoomCanvasButton(true);
                     }
                 }}
-            />
-        </CVATTooltip>
+            >
+                <ZoomInOutlined />
+                {/* <span>Undo</span> */}
+            </Button>
+        </>
     );
 }
 

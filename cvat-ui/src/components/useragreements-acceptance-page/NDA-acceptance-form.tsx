87f89a2e28d472a78f193022da872fae2be366a2
rajun918 <raju.n@ltts.com>
2022-01-18 09:00:16 +0530

    adde decline and hide saving popup trigger

--- cvat-ui/src/components/useragreements-acceptance-page/NDA-acceptance-form.tsx
+++ cvat-ui/src/components/useragreements-acceptance-page/NDA-acceptance-form.tsx
@@ -9,11 +9,11 @@ import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
 import { Document, Page, pdfjs } from 'react-pdf';
 import './userstyle.scss';
-// import FooterDrawer from 'components/login-page/intel-footer-drawer';
 import { getAcceptanceAsync } from '../../actions/acceptance-saga-action';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 import aggreementpdf from '../../assets/LTTS_freelance_agreement.pdf';
 const { Content } = Layout;
+import { logoutAsync } from 'actions/auth-saga-actions';
 /**
  * Component for displaying Terms and condition acceptance message and then redirecting to the login page
  */
@@ -27,31 +27,28 @@ function AgreementConfirmationPage(): JSX.Element {
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
     const [renderNavButtons, setRenderNavButtons] = useState<Boolean>(false);
-    const onDocumentLoadSuccess = ({numPages}): void => {
+    const onDocumentLoadSuccess = ({ numPages }): void => {
         setNumPages(numPages);
         setRenderNavButtons(true);
-      }
+    };
     const changePage = (offset: number): void => {
         setPageNumber((prevPageNumber) => prevPageNumber + offset);
     };
-    const previousPage = (): void => {
-        return changePage(-1);
-    };
-    const nextPage = (): void => {
-        return changePage(+1);
-    };
+    const previousPage = (): void => changePage(-1);
 
+    const nextPage = (): void => changePage(+1);
 
+    const Decline = (event: any) => {
+        event.preventDefault();
+        dispatch(logoutAsync());
+    };
     return (
         <Layout>
             <Content>
                 <Row justify='center' align='middle'>
                     <Col>
                         <div>
-                            <Document
-                                file={aggreementpdf}
-                                onLoadSuccess={onDocumentLoadSuccess}
-                            >
+                            <Document file={aggreementpdf} onLoadSuccess={onDocumentLoadSuccess}>
                                 <Page pageNumber={pageNumber} />
                             </Document>
                             <p justify='center' align='middle'>
@@ -63,21 +60,29 @@ function AgreementConfirmationPage(): JSX.Element {
                                         Previous Page
                                     </Button>
                                     {'  '}
-                                    <Button disabled={pageNumber === numPages} onClick={nextPage} type='default'>
-                                        Next Page
-                                    </Button>
-                                   {pageNumber === numPages && (
-                                    <Button type='primary'  onClick={(event) => acceptance(event)}>
-                                        Acceptance
-                                    </Button>
-                                      )}
+                                    {pageNumber < numPages && (
+                                        <Button disabled={pageNumber === numPages} onClick={nextPage} type='default'>
+                                            Next Page
+                                        </Button>
+                                    )}
+                                    {'  '}
+                                    {pageNumber === numPages && (
+                                        <Button type='primary' onClick={(event) => Decline(event)}>
+                                            Decline
+                                        </Button>
+                                    )}
+                                    {'  '}
+                                    {pageNumber === numPages && (
+                                        <Button type='primary' onClick={(event) => acceptance(event)}>
+                                            Acceptance
+                                        </Button>
+                                    )}
                                 </div>
                             )}
                         </div>
                     </Col>
                 </Row>
             </Content>
-            {/* <FooterDrawer /> */}
         </Layout>
     );
 }
